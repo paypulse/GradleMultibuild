8cb8c777c9c101eb8f4e9afc494d03bab20b2f91
smj <smj@useb.co.kr>
2022-07-29 18:28:40 +0900

    multimodule gradle version

---
+++ src/main/java/com/example/sringrestali/SringRestAliApplication.java
@@ -0,0 +1,13 @@
+package com.example.sringrestali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SringRestAliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SringRestAliApplication.class, args);
+    }
+
+}
